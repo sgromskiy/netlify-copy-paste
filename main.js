8394318ab22ad399c6b8f9d6f0adb944e4607a98
Sergei Gromski <sgromskiy@users.noreply.github.com>
2022-01-28 17:52:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,11 +21,15 @@ jQuery( document ).ready(function() {
     	})
     	
     	copyToClipboard(data);
-	areaCopy.val(JSON.stringify(data));
+    	areaCopy.val(JSON.stringify(data));
+    	localStorage.removeItem('NETLIFY_DATA');
+    	localStorage.setItem('NETLIFY_DATA', JSON.stringify(data));
     	}, 1000);
     	
     });
     
+    const savedData = localStorage.getItem('NETLIFY_DATA');
+    if(savedData) {areaCopy.val(savedData)};
     btnPaste.on('click', (e) => {
     	e.preventDefault();
     	const btnEdit = $("button:contains('Edit variables')");
@@ -43,14 +47,27 @@ jQuery( document ).ready(function() {
     	setTimeout(function() {
     		formRows = $('[aria-labelledby="card-title-Environment-variables"]').find('.form-row');
     		$('[aria-labelledby="card-title-Environment-variables"]').find('[aria-label="Show variable"]').trigger('click');
+    		const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
     		formRows.each((idx, row) => {
+    		
     		const rowKeyInput = $(row).find('input[name="key"]');
     		const rowValueInput = $(row).find('input[name="value"]');
-    		rowKeyInput.val(newData[idx].rowKey);
-    		rowValueInput.val(newData[idx].rowValue);
+    		
+    		nativeInputValueSetter.call(rowKeyInput[0], newData[idx].rowKey);
+		    const event1 = new Event('input', { bubbles: true });
+		    rowKeyInput[0].dispatchEvent(event1);
+		    nativeInputValueSetter.call(rowValueInput[0], newData[idx].rowValue);
+		    const event2 = new Event('input', { bubbles: true });
+		    rowValueInput[0].dispatchEvent(event2);
+    		
+    		//rowKeyInput.val(newData[idx].rowKey).prop("value", newData[idx].rowKey);
+    		//rowValueInput.val(newData[idx].rowValue).prop("value", newData[idx].rowValue);
+    		
+    		localStorage.removeItem('NETLIFY_DATA');
+    		
     	})
     	}, 2000);
-  	
+    
     });
     
     
